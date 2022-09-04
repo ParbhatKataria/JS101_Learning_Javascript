b08ddeac8069af6aa804d234d36d30c8ac1873c3
unknown <parbhatkataria628@gmail.com>
2022-09-04 13:34:13 +0530

    Javascript-V [Logical Operators ] (IW)

---
+++ Javascript-V [Logical Operators ] (IW)/problem5.js
@@ -0,0 +1,24 @@
+// Problem 5: Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday", ...
+
+let day = "sat";
+switch (day){
+  case "sun": console.log("Sunday");
+    break;
+    
+    case "mon": console.log("Monday");
+    break;
+    
+    case "tue": console.log("Tuesday");
+    break;
+    
+    case "wed": console.log("Wednesday");
+    break;
+    
+    case "thu": console.log("Thrusday");
+    break;
+    
+    case "fri": console.log("Friday");
+    break;
+    
+    case "sat": console.log("Saturday");
+}
